82b5e19e1d862c4438e7ac4cb5f8a45f2309faf8
Bogdan R <bogdan20226@gmail.com>
2022-01-18 14:45:16 +0200

    addedUsers

--- controllers/index.js
+++ controllers/index.js
@@ -1,9 +1,9 @@
 const { AuthController } = require('./auth')
 const { MoviesController } = require('./movies')
-// const { UsersController } = require('../users')
+const { UsersController } = require('./users')
 
 module.exports = [
     AuthController,
-    MoviesController
-    // UsersController
+    MoviesController,
+    UsersController
 ]
