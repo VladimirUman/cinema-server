306bb85d98ca6b80cd9387e477ee58d3657b676d
Vladimir <valodya2008@gmail.com>
2022-01-19 15:57:39 +0200

    new structure

--- controllers/index.js
+++
@@ -1,9 +0,0 @@
-const { AuthController } = require('./auth')
-const { MoviesController } = require('./movies')
-const { UsersController } = require('./users')
-
-module.exports = [
-    AuthController,
-    MoviesController,
-    UsersController
-]
